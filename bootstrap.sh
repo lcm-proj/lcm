6f1fb5a7f220397d1505194e759be198346ed923
ashuang <ashuang@989093bb-e83e-0410-a25a-9184cbcad8d0>
2008-02-14 20:34:44 +0000

    add bootstrap.sh script
    git-svn-id: https://lcm.googlecode.com/svn/trunk@112 989093bb-e83e-0410-a25a-9184cbcad8d0

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "running \"gtkdocize --copy\"..."
+gtkdocize --copy || exit 1
+
+echo "running \"autoreconf -i\"..."
+autoreconf -i || exit 1
+
+echo
+echo "Bootstrap done."
+echo
+echo "Any time configure.in or a Makefile.am changes, you must run "
+echo "    \"autoreconf -i\""
+echo ""
+echo "To build, run \"./configure ; make ; make install\""
