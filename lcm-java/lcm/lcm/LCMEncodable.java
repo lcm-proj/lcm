cfc6035171dc21c49c554c972e07e3806e76675f
edwinolson <edwinolson@989093bb-e83e-0410-a25a-9184cbcad8d0>
2008-02-16 21:04:59 +0000

    rename part 3
    git-svn-id: https://lcm.googlecode.com/svn/trunk@117 989093bb-e83e-0410-a25a-9184cbcad8d0

--- lcm-java/lcm/lcm/LCMEncodable.java
+++ lcm-java/lcm/lcm/LCMEncodable.java
@@ -1,9 +1,9 @@
-package lcm.lc;
+package lcm.lcm;
 
 import java.io.*;
 
 /** A message which can be easily sent using LC **/
-public interface LCEncodable
+public interface LCMEncodable
 {
     public void encode(DataOutputStream outs) throws IOException;
 }
