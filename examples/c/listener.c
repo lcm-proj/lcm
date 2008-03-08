1974bd677b535453d5d6ef36a4def3816bb86fb4
david.moore <david.moore@989093bb-e83e-0410-a25a-9184cbcad8d0>
2008-03-08 21:19:52 +0000

    Fix spelling error
    git-svn-id: https://lcm.googlecode.com/svn/trunk@189 989093bb-e83e-0410-a25a-9184cbcad8d0

--- examples/c/listener.c
+++ examples/c/listener.c
@@ -22,7 +22,7 @@ my_handler (const lcm_recv_buf_t *rbuf, const char * channel,
     printf ("  timestamp   = %"PRId64"\n", msg->timestamp);
     printf ("  position    = (%f, %f, %f)\n",
             msg->position[0], msg->position[1], msg->position[2]);
-    printf ("  orientaiton = (%f, %f, %f, %f)\n",
+    printf ("  orientation = (%f, %f, %f, %f)\n",
             msg->orientation[0], msg->orientation[1], msg->orientation[2],
             msg->orientation[3]);
     printf ("  ranges:");
