3235a69a8bda8e6dc649e7d2b240a77143b8cd47
edwinolson <edwinolson@989093bb-e83e-0410-a25a-9184cbcad8d0>
2008-05-01 21:15:19 +0000

    untabify
    git-svn-id: https://lcm.googlecode.com/svn/trunk@224 989093bb-e83e-0410-a25a-9184cbcad8d0

--- lcmgen/tokenize.c
+++ lcmgen/tokenize.c
@@ -14,62 +14,62 @@ static const char op_chars[] = "!~<>=&|^%*+=/";
 
 static char unescape(char c)
 {
-	switch (c) 
-	{
-	case 'n':
-		return 10;
-	case 'r':
-		return 13;
-	case 't':
-		return 9;
-	default:
-		return c;
-	}
+    switch (c) 
+    {
+    case 'n':
+        return 10;
+    case 'r':
+        return 13;
+    case 't':
+        return 9;
+    default:
+        return c;
+    }
 }
 
 tokenize_t *tokenize_create(const char *path)
 {
-	tokenize_t *t = (tokenize_t*) calloc(1, sizeof(tokenize_t));
-	t->path = strdup(path);
-	t->f = fopen(path, "r");
+    tokenize_t *t = (tokenize_t*) calloc(1, sizeof(tokenize_t));
+    t->path = strdup(path);
+    t->f = fopen(path, "r");
 
-	if (t->f == NULL) {
-		free(t->path);
-		free(t);
-		return NULL;
-	}
+    if (t->f == NULL) {
+        free(t->path);
+        free(t);
+        return NULL;
+    }
 
-	t->token = (char*) calloc(1, MAX_TOKEN_LEN);
-	t->buffer = (char*) calloc(1, MAX_LINE_LEN);
+    t->token = (char*) calloc(1, MAX_TOKEN_LEN);
+    t->buffer = (char*) calloc(1, MAX_LINE_LEN);
 
     // Line and column indices for the last returned token
-	t->token_line = -1;
-	t->token_column = -1;
+    t->token_line = -1;
+    t->token_column = -1;
 
-	t->hasnext = 0;
+    t->hasnext = 0;
 
-	t->unget_char = -1; // no currently saved char.
+    t->unget_char = -1; // no currently saved char.
 
-	return t;
+    return t;
 }
 
 void tokenize_destroy(tokenize_t *t)
 {
-	free(t->buffer);
-	free(t->token);
+    free(t->buffer);
+    free(t->token);
 
-	fclose(t->f);
-	free(t->path);
-	free(t);
+    fclose(t->f);
+    free(t->path);
+    free(t);
 }
 
 /** get the next character, counting line numbers, and columns. **/ 
 int tokenize_next_char(tokenize_t *t)
 {
-	// return an unget() char if one is stored
-	if (t->unget_char >= 0) {
+    // return an unget() char if one is stored
+    if (t->unget_char >= 0) {
 
-		t->current_char = t->unget_char;
+        t->current_char = t->unget_char;
         t->current_line = t->unget_line;
         t->current_column = t->unget_column;
         t->unget_char = -1; // mark the unget as consumed.
@@ -95,7 +95,7 @@ int tokenize_next_char(tokenize_t *t)
 
     t->buffer_column++;
 
-	return t->current_char;
+    return t->current_char;
 }
 
 int tokenize_ungetc(tokenize_t *t, int c)
@@ -104,172 +104,172 @@ int tokenize_ungetc(tokenize_t *t, int c)
     t->unget_column = t->current_column;
     t->unget_line = t->current_line;
 
-	return 0;
+    return 0;
 }
 
 void tokenize_flush_line(tokenize_t *t)
 {
-	int c;
-	do {
-		c = tokenize_next_char(t);
-	} while (c!=EOF && c!='\n');
+    int c;
+    do {
+        c = tokenize_next_char(t);
+    } while (c!=EOF && c!='\n');
 }
 
 /** chunkify tokens. **/
 int tokenize_next_internal(tokenize_t *t)
 {
-	int c;
-	int pos = 0; // output char pos
+    int c;
+    int pos = 0; // output char pos
 
 skip_white:
-	c = tokenize_next_char(t);
+    c = tokenize_next_char(t);
 
-	if (c == EOF) 
+    if (c == EOF) 
         return EOF;
 
-	if (isspace(c))
-		goto skip_white;
+    if (isspace(c))
+        goto skip_white;
 
-	// a token is starting. mark its position.
-	t->token_line = t->current_line;
-	t->token_column = t->current_column;
+    // a token is starting. mark its position.
+    t->token_line = t->current_line;
+    t->token_column = t->current_column;
 
-	// is a character literal?
-	if (c=='\'') {
-		t->token[pos++] = c;
-		c = tokenize_next_char(t);
-		if (c=='\\')
-			c = unescape(tokenize_next_char(t));
-		if (c == EOF)
-			return -4;
-		t->token[pos++] = c;
-		c = tokenize_next_char(t);
-		if (c!='\'')
-			return -5;
-		t->token[pos++] = c;
-		goto end_tok;
-	}
+    // is a character literal?
+    if (c=='\'') {
+        t->token[pos++] = c;
+        c = tokenize_next_char(t);
+        if (c=='\\')
+            c = unescape(tokenize_next_char(t));
+        if (c == EOF)
+            return -4;
+        t->token[pos++] = c;
+        c = tokenize_next_char(t);
+        if (c!='\'')
+            return -5;
+        t->token[pos++] = c;
+        goto end_tok;
+    }
 
-	// is a string literal?
-	if (c=='\"') {
-		int escape = 0;
+    // is a string literal?
+    if (c=='\"') {
+        int escape = 0;
 
-		// add the initial quote
-		t->token[pos++] = c;
+        // add the initial quote
+        t->token[pos++] = c;
 
-		// keep reading until close quote
-		while (1) {
-			if (pos >= MAX_TOKEN_LEN)
-				return -2;
+        // keep reading until close quote
+        while (1) {
+            if (pos >= MAX_TOKEN_LEN)
+                return -2;
 
-			c = tokenize_next_char(t);
-			
-			if (escape) {
-				escape = 0;
-				c = unescape(c);
+            c = tokenize_next_char(t);
+            
+            if (escape) {
+                escape = 0;
+                c = unescape(c);
 
-				continue;
-			}
+                continue;
+            }
 
-			if (c=='\"') {
-				t->token[pos++] = c;
-				goto end_tok;
-			}
-			if (c=='\\') {
-				escape = 1;
-				continue;
-			}
+            if (c=='\"') {
+                t->token[pos++] = c;
+                goto end_tok;
+            }
+            if (c=='\\') {
+                escape = 1;
+                continue;
+            }
 
-			t->token[pos++] = c;
-		}
-		goto end_tok;
-	}
+            t->token[pos++] = c;
+        }
+        goto end_tok;
+    }
 
-	// is an operator?
-	if (strchr(op_chars, c)!=NULL) {
-		while (strchr(op_chars, c)!=NULL) {
-			if (pos >= MAX_TOKEN_LEN)
-				return -2;
-			t->token[pos++] = c;
-			c = tokenize_next_char(t);
-		}
-		tokenize_ungetc(t, c);
-		goto end_tok;
-	}
+    // is an operator?
+    if (strchr(op_chars, c)!=NULL) {
+        while (strchr(op_chars, c)!=NULL) {
+            if (pos >= MAX_TOKEN_LEN)
+                return -2;
+            t->token[pos++] = c;
+            c = tokenize_next_char(t);
+        }
+        tokenize_ungetc(t, c);
+        goto end_tok;
+    }
 
-	// otherwise, all tokens are alpha-numeric blobs
+    // otherwise, all tokens are alpha-numeric blobs
 in_tok:
-	if (pos >= MAX_TOKEN_LEN)
-		return -2;
+    if (pos >= MAX_TOKEN_LEN)
+        return -2;
 
-	t->token[pos++] = c;
-	
-	if (strchr(single_char_toks,c)!=NULL)
-		goto end_tok;
+    t->token[pos++] = c;
+    
+    if (strchr(single_char_toks,c)!=NULL)
+        goto end_tok;
 
-	c = tokenize_next_char(t);
-	if (strchr(single_char_toks,c)!=NULL ||
-	    strchr(op_chars,c)!=NULL) {
-		tokenize_ungetc(t, c);
-		goto end_tok;
-	}
-	
-	if (!isspace(c) && c != EOF)
-		goto in_tok;
+    c = tokenize_next_char(t);
+    if (strchr(single_char_toks,c)!=NULL ||
+        strchr(op_chars,c)!=NULL) {
+        tokenize_ungetc(t, c);
+        goto end_tok;
+    }
+    
+    if (!isspace(c) && c != EOF)
+        goto in_tok;
 
 end_tok:
-	t->token[pos] = 0;
+    t->token[pos] = 0;
 
-	return pos;
+    return pos;
 }
 
 /** remove comments **/
 int tokenize_next(tokenize_t *t)
 {
-	int res;
+    int res;
 
-	if (t->hasnext) {
-		t->hasnext = 0;
-		return 0;
-	}
+    if (t->hasnext) {
+        t->hasnext = 0;
+        return 0;
+    }
 
 restart:
-	res = tokenize_next_internal(t);
+    res = tokenize_next_internal(t);
 
-	if (res == EOF)
-		return EOF;
+    if (res == EOF)
+        return EOF;
 
-	// block comment?
-	if (!strncmp(t->token,"/*",2)) {
-		while (1) {
-			res = tokenize_next_internal(t);
-			if (res == EOF)
-				return -10;
-			int len = strlen(t->token);
-			if (len >= 2 && !strncmp(&t->token[len-2],"*/", 2))
-				break;
-		}
-		goto restart;
-	}
+    // block comment?
+    if (!strncmp(t->token,"/*",2)) {
+        while (1) {
+            res = tokenize_next_internal(t);
+            if (res == EOF)
+                return -10;
+            int len = strlen(t->token);
+            if (len >= 2 && !strncmp(&t->token[len-2],"*/", 2))
+                break;
+        }
+        goto restart;
+    }
 
-	// end of line comment? If so, instantly consume the rest of this line.
-	if (!strncmp(t->token,"//", 2)) {
-		tokenize_flush_line(t);
-		goto restart;
-	}
+    // end of line comment? If so, instantly consume the rest of this line.
+    if (!strncmp(t->token,"//", 2)) {
+        tokenize_flush_line(t);
+        goto restart;
+    }
 
-	return res;
+    return res;
 }
 
 int tokenize_peek(tokenize_t *t)
 {
-	int res = 0;
+    int res = 0;
 
-	if (t->hasnext)
-		return res;
+    if (t->hasnext)
+        return res;
 
-	res = tokenize_next(t);
-	t->hasnext = 1;
+    res = tokenize_next(t);
+    t->hasnext = 1;
 
-	return res;
+    return res;
 }
