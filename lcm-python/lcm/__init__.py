8fc2cd32ba02d1fe2216c54d9655659d53db4256
ashuang <ashuang@989093bb-e83e-0410-a25a-9184cbcad8d0>
2009-04-14 16:20:09 +0000

    lcm-python remove pure python log class, add some comment docs
    git-svn-id: https://lcm.googlecode.com/svn/trunk@356 989093bb-e83e-0410-a25a-9184cbcad8d0

--- lcm-python/lcm/__init__.py
+++ lcm-python/lcm/__init__.py
@@ -12,8 +12,8 @@ class Event:
         """Event number"""
 
         self.timestamp = timestamp 
-        """Microseconds since the epoch marking the time at which the message
-        was originally received
+        """Microseconds since 00:00:00 Jan 1, 1970 UTC marking the time at
+        which the message was originally received
         """
         self.channel = channel
         """Channel on which message was received"""
@@ -50,15 +50,38 @@ to next() returning the next Event in the log.
         self.f = None
 
     def seek (self, filepos):
+        """seek(filepos) --> None
+        @param filepos: byte offset from start of log
+
+        Positions the internal file pointer at the next event that
+        starts at or is after byte offset filepos.
+        """
         return self.c_eventlog.seek (filepos)
 
     def size (self):
+        """size() --> int
+
+        @return the total size of the log file, in bytes
+        """
         return self.c_eventlog.size ()
 
     def close (self):
+        """close() --> None
+
+        Closes the log file.  After an EventLog is closed, it is essentially
+        useless
+        """
         return self.c_eventlog.close ()
 
     def write_event (self, utime, channel, data):
+        """write_event(utime, channel, data) --> None
+        @param utime:    Microseconds since 00:00:00 Jan 1, 1970 UTC
+        @param channel:  Channel name corresponding to the event
+        @param data:     data bytes
+
+        Writes an event to the log file.  Log file must be openeed in write
+        mode.
+        """
         return self.c_eventlog.write_event (utime, channel, data)
 
     def read_next_event (self):
@@ -77,74 +100,8 @@ to next() returning the next Event in the log.
         return next_evt
 
     def tell (self):
+        """tell() --> int
+
+        @return the current position of the internal file pointer
+        """
         return self.c_eventlog.ftell ()
-
-EVENTLOG_MAGIC = 0xEDA1DA01L
-
-class _PyEventLog:
-    def __init__ (self, path, mode = "r", overwrite = False):
-        if mode not in [ "r", "w" ]:
-            raise ValueError ("invalid event log mode")
-
-        if mode == "w" and os.path.exists(path) and not overwrite:
-            raise ValueError ("Refusing to overwrite existing log file "
-                    "unless overwrite is set to True")
-        self.mode = mode
-        self.c_eventlog = None
-        self.f = file (path, mode)
-
-    def seek (self, filepos):
-        if self.mode != "r":
-            raise RuntimeError ("seek not supported in write mode")
-        self.f.seek (filepos)
-
-    def size (self):
-        if self.mode == "r":
-            curpos = self.f.tell ()
-            self.f.seek (0, 2)
-            result = self.f.tell ()
-            self.f.seek (curpos, 0)
-            return result
-        else:
-            return self.f.tell ()
-
-    def close (self):
-        self.f.close ()
-
-    def write_event (self, utime, channel, data):
-        raise NotImplementedError ()
-
-    def read_next_event (self):
-        if self.mode != "r":
-            raise RuntimeError("read not supported in write mode")
-        testmagic = 0
-        while testmagic != MAGIC:
-            byte = self.f.read (1)
-            if len(byte) == 0: return None
-
-            r = struct.unpack ("B", byte)[0]
-            testmagic = ((testmagic << 8) & 0xffffffff) | r
-
-        fmt = ">qqii"
-        header = self.f.read (struct.calcsize (fmt))
-        eventnum, timestamp, channellen, datalen = \
-                struct.unpack (fmt, header)
-        channel = self.f.read (channellen)
-        if len(channel) != channellen: return None
-        data = self.f.read (datalen)
-        if len(data) != datalen: return None
-
-        return Event (eventnum, timestamp, channel, data)
-    
-    def __iter__ (self):
-        self.f.seek (0)
-        return self
-    
-    def next (self):
-        next_evt = self.read_next_event ()
-        if not next_evt:
-            raise StopIteration
-        return next_evt
-
-    def tell (self):
-        return self.f.tell ()
