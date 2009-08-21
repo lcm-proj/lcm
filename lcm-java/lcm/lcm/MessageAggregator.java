87f5b68357cd47a7f1d81a320324e7345eb5674d
ashuang <ashuang@989093bb-e83e-0410-a25a-9184cbcad8d0>
2009-08-21 20:02:55 +0000

    Deque -> LinkedList
    git-svn-id: https://lcm.googlecode.com/svn/trunk@394 989093bb-e83e-0410-a25a-9184cbcad8d0

--- lcm-java/lcm/lcm/MessageAggregator.java
+++ lcm-java/lcm/lcm/MessageAggregator.java
@@ -40,7 +40,7 @@ public class MessageAggregator
 // Would prefer to use ArrayDequeue for performance reasons, but it's too new
 // (only since Java 1.6)
 //    Deque<Message> messages = new ArrayDeque<Message>();
-    Deque<Message> messages = new LinkedList<Message>();
+    LinkedList<Message> messages = new LinkedList<Message>();
 
     long queue_data_size = 0;
     long max_queue_data_size = 100 * (1 << 20); // 100 megabytes
