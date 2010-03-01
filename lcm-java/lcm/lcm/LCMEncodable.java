c1044fc74fb4b9232a3426b420018333a98d2a0f
edwinolson <edwinolson@989093bb-e83e-0410-a25a-9184cbcad8d0>
2010-03-01 21:25:24 +0000

    expose recursive encode/decode methods for advanced users
    git-svn-id: https://lcm.googlecode.com/svn/trunk@441 989093bb-e83e-0410-a25a-9184cbcad8d0

--- lcm-java/lcm/lcm/LCMEncodable.java
+++ lcm-java/lcm/lcm/LCMEncodable.java
@@ -6,9 +6,24 @@ import java.io.*;
  **/
 public interface LCMEncodable
 {
+    /** LCMEncodables will always have an empty constructor and a
+     * constructor that takes a DataInput. **/
+
     /**
      * Invoked by LCM.
      * @param outs Any data to be sent should be written to this output stream.
      */
     public void encode(DataOutput outs) throws IOException;
+
+
+    /** Encode the data without the magic header. Most users will
+     * never use this function.
+     **/
+    public void _encodeRecursive(DataOutput outs) throws IOException;
+
+    /** Decode the data without the magic header. Most users will
+     * never use this function.
+     **/
+    public void _decodeRecursive(DataInput ins) throws IOException;
+
 }
