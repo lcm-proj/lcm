d63f81dea3038cfd338939fc6969f6fd98e15b24
jan.hrbacek <jan.hrbacek@989093bb-e83e-0410-a25a-9184cbcad8d0>
2010-06-20 21:36:09 +0000

    .NET port basic tutorial + additional temperature_t examples
    git-svn-id: https://lcm.googlecode.com/svn/trunk@478 989093bb-e83e-0410-a25a-9184cbcad8d0

---
+++ examples/csharp/temperature_t_demo/temperature_t_display.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using LCM.LCM;
+using LCMTypes;
+
+namespace LCM.Examples
+{
+    /// <summary>
+    /// Demo listener, see LCM .NET tutorial for more information
+    /// </summary>
+    class TemperatureDisplay : LCMSubscriber
+    {
+        public void MessageReceived(LCM.LCM lcm, string channel, LCMDataInputStream ins)
+        {
+            if (channel == "HALLWAY_TEMPERATURE")
+            {
+                try
+                {
+                    temperature_t temp = new temperature_t(ins);
+                    Console.WriteLine("The temperature is: " + temp.deg_celsius);
+                }
+                catch (System.IO.IOException ex)
+                {
+                    Console.Error.WriteLine("Error decoding temperature message: " + ex);
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            LCM.LCM myLCM;
+
+            try
+            {
+                myLCM = new LCM.LCM();
+
+                myLCM.Subscribe("HALLWAY_TEMPERATURE", new TemperatureDisplay());
+
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Ex: " + ex);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ examples/csharp/temperature_t_demo/temperature_t_transmit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using LCM.LCM;
+using LCMTypes;
+
+namespace LCM.Examples
+{
+    /// <summary>
+    /// Demo transmitter, see LCM .NET tutorial for more information
+    /// </summary>
+    class TemperatureTransmit
+    {
+        public static void Main(string[] args)
+        {
+            LCM.LCM myLCM = LCM.LCM.Singleton;
+
+            while (true)
+            {
+                try
+                {
+                    temperature_t temp = new temperature_t();
+                    temp.utime = DateTime.Now.Ticks / 10;
+                    temp.deg_celsius = 25.0 + 5 * Math.Sin(DateTime.Now.Ticks / 10000000.0);
+
+                    myLCM.Publish("HALLWAY_TEMPERATURE", temp);
+
+                    System.Threading.Thread.Sleep(1000);
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine("Ex: " + ex);
+                }
+            }
+        }
+    }
+}
