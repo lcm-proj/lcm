d8d7002586400dbb14764cfc230ef1eee9e4cb60
ashuang <ashuang@989093bb-e83e-0410-a25a-9184cbcad8d0>
2010-11-30 22:55:24 +0000

    ClassDiscoverer searches directories on classpath for .class files, in addition to .jar files.
    git-svn-id: https://lcm.googlecode.com/svn/trunk@524 989093bb-e83e-0410-a25a-9184cbcad8d0

--- lcm-java/lcm/util/ClassDiscoverer.java
+++ lcm-java/lcm/util/ClassDiscoverer.java
@@ -21,6 +21,30 @@ public class ClassDiscoverer
         findClasses(cp, visitor);
     }
 
+    private static void visitDirectory(ClassVisitor visitor, URLClassLoader cldr, 
+            String classpath_entry, File dir, String visiting_classpath) {
+        for(File f : dir.listFiles()) {
+            String fname = f.getName();
+            if(f.isDirectory()) {
+                // found a directory. recursively traverse the directory and
+                // search for .class files
+                if(fname.contains("."))
+                    continue;
+
+                visitDirectory(visitor, cldr, classpath_entry, f, fname + ".");
+            } else if(f.isFile() && fname.endsWith(".class")) {
+                // found a .class file.  Construct its full classname and pass
+                // it to the class visitor
+                String cn = visiting_classpath + fname.substring(0, fname.length()-6);
+                try {
+                    Class cls = cldr.loadClass(cn);
+                    if (cls != null)
+                        visitor.classFound(classpath_entry, cls);
+                } catch (Throwable ex) { }
+            }
+        }
+    }
+
     /** Given a colon-delimited list of jar files, iterate over the
      * classes in them.
      * @param cp The colon-deliimited classpath to search
@@ -47,51 +71,54 @@ public class ClassDiscoverer
         }
 
         for (int i = 0; i < items.length; i++) {
-
             String item = items[i];
 
-            if (!item.endsWith(".jar") || !(new File(item).exists()))
-                continue;
+            if (item.endsWith(".jar")) {
+                try {
+                    JarFile jf = new JarFile(item);
 
-            try {
-                JarFile jf = new JarFile(item);
+                    for (Enumeration<JarEntry> e = jf.entries() ; e.hasMoreElements() ;) {
 
-                for (Enumeration<JarEntry> e = jf.entries() ; e.hasMoreElements() ;) {
+                        JarEntry je = e.nextElement();
 
-                    JarEntry je = e.nextElement();
+                        String n = je.getName();
 
-                    String n = je.getName();
+                        // skip private classes?
+                        //		    if (n.contains("$"))
+                        //			continue;
 
-                    // skip private classes?
-                    //		    if (n.contains("$"))
-                    //			continue;
+                        if (n.endsWith(".class")) {
 
-                    if (n.endsWith(".class")) {
+                            // convert the path into a class name
+                            String cn = n.substring(0, n.length()-6);
+                            cn = cn.replace('/', '.');
+                            cn = cn.replace('\\', '.');
 
-                        // convert the path into a class name
-                        String cn = n.substring(0, n.length()-6);
-                        cn = cn.replace('/', '.');
-                        cn = cn.replace('\\', '.');
+                            // try loading that class
+                            try {
+                                Class cls = cldr.loadClass(cn);
 
-                        // try loading that class
-                        try {
-                            Class cls = cldr.loadClass(cn);
+                                if (cls == null)
+                                    continue;
 
-                            if (cls == null)
-                                continue;
+                                visitor.classFound(item, cls);
 
-                            visitor.classFound(item, cls);
-
-                        } catch (Throwable ex) {
-                            System.out.println("ClassDiscoverer: "+ex);
-                            System.out.println("                 jar: "+item);
-                            System.out.println("                 class: "+n);
+                            } catch (Throwable ex) {
+                                System.out.println("ClassDiscoverer: "+ex);
+                                System.out.println("                 jar: "+item);
+                                System.out.println("                 class: "+n);
+                            }
                         }
                     }
-                }
 
-            } catch(IOException ioe) {
-                System.out.println("Error extracting "+items[i]);
+                } catch(IOException ioe) {
+                    System.out.println("Error extracting "+items[i]);
+                }
+            } else {
+                File f = new File(item);
+                if(!f.isDirectory())
+                    continue;
+                visitDirectory(visitor, cldr, item, f, "");
             }
         }
     }
