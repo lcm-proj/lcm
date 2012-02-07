aecd1ea905542997122a2ad947db28c7eeb0bc7a
ashuang <ashuang@989093bb-e83e-0410-a25a-9184cbcad8d0>
2012-02-07 03:58:53 +0000

    lcm-python: fix setup.py for linux
    git-svn-id: https://lcm.googlecode.com/svn/branches/py-win@642 989093bb-e83e-0410-a25a-9184cbcad8d0

--- lcm-python/setup.py
+++ lcm-python/setup.py
@@ -6,78 +6,83 @@ import os
 sources = [ \
     "module.c",
     "pyeventlog.c",
-    "pylcm.c", 
+    "pylcm.c",
     "pylcm_subscription.c",
-    os.path.join("..", "lcm", "lcm.c"), 
-    os.path.join("..", "lcm", "lcm_udp.c"), 
-    os.path.join("..", "lcm", "lcm_file.c"), 
-    os.path.join("..", "lcm", "lcm_tcpq.c"), 
-    os.path.join("..", "lcm", "ringbuffer.c"), 
+    os.path.join("..", "lcm", "lcm.c"),
+    os.path.join("..", "lcm", "lcm_udp.c"),
+    os.path.join("..", "lcm", "lcm_file.c"),
+    os.path.join("..", "lcm", "lcm_tcpq.c"),
+    os.path.join("..", "lcm", "ringbuffer.c"),
     os.path.join("..", "lcm", "eventlog.c") ]
+
+include_dirs = []
+define_macros = []
+library_dirs = []
+libraries = []
+extra_compile_args = []
 
 if os.name == 'nt':
     # check for GLIB_PATH environment var, exit with error if not found
     glibPath = os.getenv('GLIB_PATH')
-    if not glibPath: 
+    if not glibPath:
         sys.exit('GLIB_PATH environment variable not set.')
 
-    includeDirs = [ \
+    include_dirs = [ \
             "..",
             os.path.join("..", "WinSpecific\include"),
             os.path.join("..", "WinSpecific"),
             os.path.join(glibPath, "include", "glib-2.0"),
             os.path.join(glibPath, "lib", "glib-2.0", "include") ]
-    libraryDirs = [ \
-            os.path.join(glibPath, 'lib'),
-            ]
+    library_dirs.append(os.path.join(glibPath, 'lib'))
 
     # define additional macro WIN32, used to discriminate win specific code
-    defineMacros = [('WIN32', 1)]
+    define_macros = [('WIN32', 1)]
 
-    winlibs = [ 'Ws2_32', 'glib-2.0', 'gthread-2.0' ]
+    libraries = [ 'Ws2_32', 'glib-2.0', 'gthread-2.0' ]
 
     # compiler arguments
     # /TP enforces compilation of code as c++
-    extraCompileArgs = [ '/TP' ]
+    extra_compile_args = [ '/TP' ]
 
     # we need to patch the msvccompiler.MSVCCompiler class to compile
     # .c C files as C++ code (/TP switch for MSVC)
-    # the default behaviour generates the command line switch /Tc for 
+    # the default behaviour generates the command line switch /Tc for
     # every .c source file
     msvccompiler.MSVCCompiler._c_extensions = []
     msvccompiler.MSVCCompiler._cpp_extensions.append('.c')
 
-    pylcm_extension = Extension("lcm._lcm", 
-                sources + \
-                    [ os.path.join("..", "WinSpecific", "WinPorting.cpp") ],
-            include_dirs=includeDirs,
-            define_macros=defineMacros,
-            library_dirs=libraryDirs,
-            libraries=winlibs,
-            extra_compile_args=extraCompileArgs)
-else:
-    pkgconfig_cflags = commands.getoutput ("pkg-config --cflags glib-2.0 gthread-2.0")
-    pkgconfig_include_flags = commands.getoutput ("pkg-config --cflags-only-I")
-    pkgconfig_include_dirs = [ t[2:] for t in pkgconfig_include_flags.split() ]
+    sources.append(os.path.join("..", "WinSpecific", "WinPorting.cpp"))
 
-    pkgconfig_lflags = commands.getoutput ( \
-            "pkg-config --libs-only-l glib-2.0 gthread-2.0")
-    pkgconfig_libs = [ t[2:] for t in pkgconfig_lflags.split() ]
-  
-    pkgconfig_biglflags = commands.getoutput ( \
-            "pkg-config --libs-only-L glib-2.0 gthread-2.0")
-    pkgconfig_ldirs = [ t[2:] for t in pkgconfig_biglflags.split() ]
-  
-    pylcm_extension = Extension("lcm._lcm",
-            sources=sources,
-            library_dirs=pkgconfig_ldirs,
-            include_dirs=pkgconfig_include_dirs,
-            libraries=pkgconfig_libs,
-            extra_compile_args=['-Wno-strict-prototypes',
-                pkgconfig_cflags,
-                "-D_FILE_OFFSET_BITS=64",
-                "-D_LARGEFILE_SOURCE",
-                "-std=gnu99" ])
+else:
+    pkg_deps = "glib-2.0 gthread-2.0"
+
+    # include path
+    pkgconfig_include_flags = commands.getoutput("pkg-config --cflags-only-I")
+    include_dirs = [ t[2:] for t in pkgconfig_include_flags.split() ]
+
+    # libraries
+    pkgconfig_lflags = commands.getoutput("pkg-config --libs-only-l %s" % pkg_deps)
+    libraries = [ t[2:] for t in pkgconfig_lflags.split() ]
+
+    # link directories
+    pkgconfig_biglflags = commands.getoutput("pkg-config --libs-only-L %s" % pkg_deps)
+    library_dirs = [ t[2:] for t in pkgconfig_biglflags.split() ]
+
+    # other compiler flags
+    pkgconfig_cflags = commands.getoutput("pkg-config --cflags %s" % pkg_deps).split()
+    extra_compile_args = [ \
+        '-Wno-strict-prototypes',
+        "-D_FILE_OFFSET_BITS=64",
+        "-D_LARGEFILE_SOURCE",
+        "-std=gnu99" ] + pkgconfig_cflags
+
+pylcm_extension = Extension("lcm._lcm",
+        sources,
+        include_dirs=include_dirs,
+        define_macros=define_macros,
+        library_dirs=library_dirs,
+        libraries=libraries,
+        extra_compile_args=extra_compile_args)
 
 setup(name="lcm", version="0.8.0",
       ext_modules=[pylcm_extension],
