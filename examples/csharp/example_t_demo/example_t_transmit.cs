b992378e3328b29d1bb1c7e045fbe24f0583a9cb
jan.hrbacek <jan.hrbacek@989093bb-e83e-0410-a25a-9184cbcad8d0>
2012-10-04 10:21:41 +0000

    modify one of the c# examples so that it can be easily used to test fragmented messages (by increasing num_ranges)
    git-svn-id: https://lcm.googlecode.com/svn/trunk@690 989093bb-e83e-0410-a25a-9184cbcad8d0

--- examples/csharp/example_t_demo/example_t_transmit.cs
+++ examples/csharp/example_t_demo/example_t_transmit.cs
@@ -22,8 +22,12 @@ namespace LCM.Examples
                 msg.timestamp = span.Ticks * 100;
                 msg.position = new double[] { 1, 2, 3 };
                 msg.orientation = new double[] { 1, 0, 0, 0 };
-                msg.ranges = new short[] { 0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
-                msg.num_ranges = msg.ranges.Length;
+                msg.num_ranges = 15;
+                msg.ranges = new short[msg.num_ranges];
+                for (int i = 0; i < msg.num_ranges; i++)
+                {
+                    msg.ranges[i] = (short) i;
+                }
                 msg.name = "example string";
                 msg.enabled = true;
 
