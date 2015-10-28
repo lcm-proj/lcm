8b8217b392996bd5a88c900f59c5fcef9384c51e
Albert Huang <ashuang@gmail.com>
2015-10-28 09:56:41 -0700

    Release 1.3.0

--- lcm/lcm.h
+++ lcm/lcm.h
@@ -348,10 +348,10 @@ int lcm_subscription_set_queue_capacity(lcm_subscription_t* handler, int num_mes
 #define LCM_MAJOR_VERSION 1
 
 /// LCM release minor version - the Y in version X.Y.Z
-#define LCM_MINOR_VERSION 2
+#define LCM_MINOR_VERSION 3
 
 /// LCM release micro version - the Z in version X.Y.Z
-#define LCM_MICRO_VERSION 1
+#define LCM_MICRO_VERSION 0
 
 /**
  * @}
