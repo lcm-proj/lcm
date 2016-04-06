fd9886c5c195a204956be6a4b81793a7842c268b
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-04-06 16:19:56 -0400

    Add lcm-gen to CMake build

--- lcm-cmake/install.cmake
+++ lcm-cmake/install.cmake
@@ -20,7 +20,11 @@ write_basic_package_version_file(
 )
 
 # Exported targets for build directory
-export(TARGETS lcm FILE ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Targets.cmake)
+export(TARGETS
+  lcm
+  lcm-gen
+  FILE ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Targets.cmake
+)
 
 # Exported targets for installation
 install(EXPORT ${PROJECT_NAME}Targets
