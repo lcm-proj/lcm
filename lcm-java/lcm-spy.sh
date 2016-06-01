31f948e95d7b8562f97e71d88ebe9873932ebecd
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-06-01 13:03:33 -0400

    Add command line utilities
    Add the command line utilities lcm-spy and lcm-logplayer-gui. Since
    CMake encourages relocatable installations, simply hard-coding the full
    path to the jar is inadequate. Instead, try to determine the path of the
    script (using perl if available; otherwise, invoking the tools via a
    symlink in a different directory will fail) and the jar file (so the
    scripts can be run in the build directory as well as in an install). As
    a result, the scripts don't actually change depending on the build
    environment, so the "final" scripts can be kept in the source directory
    and just copied as needed.
    
    Also, fix the name of the JAR (oops).

---
+++ lcm-java/lcm-spy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ -n "$(type -p perl)" ]
+  then mydir="$(dirname "$(perl -MCwd -le 'print Cwd::abs_path(shift)' "${BASH_SOURCE[0]}")")"
+  else mydir="$(dirname "${BASH_SOURCE[0]}")"
+fi
+if [ -e "$mydir/lcm.jar" ]
+  then jardir="$mydir"
+elif [ -e "$mydir/../share/java/lcm.jar" ]
+  then jardir="$mydir/../share/java"
+else
+  echo "Unable to find 'lcm.jar'; please check your installation" >&2
+  exit 1
+fi
+
+exec java -server -Djava.net.preferIPv4Stack=true -Xincgc -Xmx128m -Xms64m -ea -cp $jardir/lcm.jar lcm.spy.Spy "$@"
