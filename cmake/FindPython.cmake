a90c7328904a9c04e0c8be5a82e0b3fef9d790ff
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-11-18 14:52:29 -0500

    Fix finding Python 3
    Fix small error in FindPython.cmake that would cause it to break with
    Python 3.

--- cmake/FindPython.cmake
+++ cmake/FindPython.cmake
@@ -3,7 +3,7 @@ include(FindPackageHandleStandardArgs)
 find_package(PythonInterp)
 if(PYTHON_EXECUTABLE)
   execute_process(
-    COMMAND ${PYTHON_EXECUTABLE} -c "import sys; print sys.exec_prefix;"
+    COMMAND ${PYTHON_EXECUTABLE} -c "import sys; print(sys.exec_prefix);"
     OUTPUT_VARIABLE PYTHON_PREFIX
     OUTPUT_STRIP_TRAILING_WHITESPACE)
   list(APPEND CMAKE_PREFIX_PATH ${PYTHON_PREFIX})
