a91d8036fe593e68bdb2ecd46a15620b635adfbc
Tim Perkins <tprk77@gmail.com>
2018-07-25 19:44:49 -0400

    Updated the formatting script

--- format_code.sh
+++ format_code.sh
@@ -24,14 +24,33 @@
 
 LCM_ROOT="$(cd $(dirname "$0") && pwd)"
 
+# format_c_cpp_dir_r DIR [EXCUDE_PATTERN...]
+#
+# Formats all C and C++ sources in a directory recursively. DIR is the source
+# directory for the recursive formatting. The optional EXCLUDE_PATTERN is an
+# Extended Regex matched against each file path, where the file is excluded from
+# formatting if it matches. There can be multiple exclude patterns.
+
 function format_c_cpp_dir_r {
-    find "$1" -regex '.*\.\(c\|h\)\(pp\)?' -print0 | xargs -0 clang-format -i
+    include_dir="$1"
+    exclude_patterns=("${@:2}")
+    if [ "${#exclude_patterns[@]}" -eq 0 ]; then
+        # Use an empty pattern to not match anything
+        exclude_patterns="^$"
+    fi
+    exclude_pattern_opts=""
+    for exclude_pattern in "${exclude_patterns[@]}"; do
+        exclude_pattern_opts="${exclude_pattern_opts} -e \"${exclude_pattern}\" "
+    done
+    find "$1" -regex '.*\.\(c\|h\)\(pp\)?' \
+        | eval "grep -E -v" "${exclude_pattern_opts}" \
+        | xargs clang-format -i
 }
 
-format_c_cpp_dir_r "${LCM_ROOT}/lcm"
+format_c_cpp_dir_r "${LCM_ROOT}/lcm" "/lcmtypes/"
 format_c_cpp_dir_r "${LCM_ROOT}/lcmgen"
-#format_c_cpp_dir_r "${LCM_ROOT}/lcm-examples"
-#format_c_cpp_dir_r "${LCM_ROOT}/lcm-test"
+format_c_cpp_dir_r "${LCM_ROOT}/examples"
+format_c_cpp_dir_r "${LCM_ROOT}/test" "/gtest/"
 #format_c_cpp_dir_r "${LCM_ROOT}/lcm-logger"
 #format_c_cpp_dir_r "${LCM_ROOT}/lcm-python"
 #format_c_cpp_dir_r "${LCM_ROOT}/lcm-lua"
