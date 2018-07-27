495ec10b42ba5e52a24cf810a04bc47751e470f3
Tim Perkins <tprk77@gmail.com>
2018-07-26 20:37:05 -0400

    Applied formatting

--- lcm-logger/glib_util.c
+++ lcm-logger/glib_util.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
 #include <fcntl.h>
-#include <string.h>
-#include <stdlib.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #ifdef WIN32
 #define SIGKILL 2
 #include <lcm/windows/WinPorting.h>
 //#include <Winsock2.h>
 #else
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 #define lcm_internal_pipe_write write
 #define lcm_internal_pipe_read read
 #define lcm_internal_pipe_close close
@@ -36,32 +36,30 @@ static int g_sp_initialized = 0;
 
 extern GMainLoop *_mainloop;
 
-int 
-signal_pipe_init()
+int signal_pipe_init()
 {
     if (g_sp_initialized) {
         fprintf(stderr, "signal_pipe already initialized!!\n");
         return -1;
     }
 
-    if (0 != lcm_internal_pipe_create (g_sp.fds)) {
+    if (0 != lcm_internal_pipe_create(g_sp.fds)) {
         perror("signal_pipe");
         return -1;
     }
-    int flags = fcntl (g_sp.fds[1], F_GETFL);
-    fcntl (g_sp.fds[1], F_SETFL, flags | O_NONBLOCK);
+    int flags = fcntl(g_sp.fds[1], F_GETFL);
+    fcntl(g_sp.fds[1], F_SETFL, flags | O_NONBLOCK);
     g_sp_initialized = 1;
 
     return 0;
 }
 
-int 
-signal_pipe_cleanup()
+int signal_pipe_cleanup()
 {
     if (g_sp_initialized) {
-        lcm_internal_pipe_close (g_sp.fds[0]);
-        lcm_internal_pipe_close (g_sp.fds[1]);
-        g_io_channel_unref (g_sp.ioc);
+        lcm_internal_pipe_close(g_sp.fds[0]);
+        lcm_internal_pipe_close(g_sp.fds[1]);
+        g_io_channel_unref(g_sp.ioc);
         g_sp_initialized = 0;
         return 0;
     }
@@ -69,36 +67,32 @@ signal_pipe_cleanup()
     return -1;
 }
 
-static void
-signal_handler (int signum)
+static void signal_handler(int signum)
 {
-    size_t status = lcm_internal_pipe_write (g_sp.fds[1], &signum, sizeof(int));
-    (void)status;
+    size_t status = lcm_internal_pipe_write(g_sp.fds[1], &signum, sizeof(int));
+    (void) status;
 }
 
-static int
-signal_handler_glib (GIOChannel *source, GIOCondition condition, void *ud)
+static int signal_handler_glib(GIOChannel *source, GIOCondition condition, void *ud)
 {
     int signum;
-    size_t status = lcm_internal_pipe_read (g_sp.fds[0], &signum, sizeof(int));
+    size_t status = lcm_internal_pipe_read(g_sp.fds[0], &signum, sizeof(int));
 
     if (status != sizeof(int)) {
-        fprintf(stderr, "wtf!? signal_handler_glib is confused (%s:%d)\n", 
-                __FILE__, __LINE__);
+        fprintf(stderr, "wtf!? signal_handler_glib is confused (%s:%d)\n", __FILE__, __LINE__);
         return TRUE;
     }
 
     if (g_sp.userfunc) {
-        g_sp.userfunc (signum, g_sp.userdata);
+        g_sp.userfunc(signum, g_sp.userdata);
     }
 
     return TRUE;
 }
 
-void 
-signal_pipe_add_signal (int sig)
+void signal_pipe_add_signal(int sig)
 {
-    // TODO use sigaction instead of signal()
+// TODO use sigaction instead of signal()
 #if 0
     struct sigaction siga;
     siga.sa_handler = signal_handler;
@@ -112,24 +106,25 @@ signal_pipe_add_signal (int sig)
         perror("signal_pipe: sigaction failed");
     }
 #else
-    signal (sig, signal_handler);
+    signal(sig, signal_handler);
 #endif
 
     return;
 }
 
-int 
-signal_pipe_attach_glib (signal_pipe_glib_handler_t func, gpointer user_data)
+int signal_pipe_attach_glib(signal_pipe_glib_handler_t func, gpointer user_data)
 {
-    if (! g_sp_initialized) return -1;
+    if (!g_sp_initialized)
+        return -1;
 
-    if (g_sp.ioc) return -1;
+    if (g_sp.ioc)
+        return -1;
 
-    g_sp.ioc = g_io_channel_unix_new (g_sp.fds[0]);
-    g_io_channel_set_flags (g_sp.ioc, 
-            (GIOFlags) (g_io_channel_get_flags (g_sp.ioc) | G_IO_FLAG_NONBLOCK), NULL);
-    g_sp.ios = g_io_add_watch (g_sp.ioc, (GIOCondition) (G_IO_IN | G_IO_PRI), 
-            (GIOFunc) signal_handler_glib, NULL);
+    g_sp.ioc = g_io_channel_unix_new(g_sp.fds[0]);
+    g_io_channel_set_flags(g_sp.ioc,
+                           (GIOFlags)(g_io_channel_get_flags(g_sp.ioc) | G_IO_FLAG_NONBLOCK), NULL);
+    g_sp.ios = g_io_add_watch(g_sp.ioc, (GIOCondition)(G_IO_IN | G_IO_PRI),
+                              (GIOFunc) signal_handler_glib, NULL);
 
     g_sp.userfunc = func;
     g_sp.userdata = user_data;
@@ -137,29 +132,26 @@ signal_pipe_attach_glib (signal_pipe_glib_handler_t func, gpointer user_data)
     return 0;
 }
 
-
-static void
-spgqok_handler (int signum, void *user)
+static void spgqok_handler(int signum, void *user)
 {
-    g_main_loop_quit (_mainloop);
+    g_main_loop_quit(_mainloop);
     signal_pipe_cleanup();
 }
 
-int 
-signal_pipe_glib_quit_on_kill ()
+int signal_pipe_glib_quit_on_kill()
 {
-    if (0 != signal_pipe_init()) return -1;
+    if (0 != signal_pipe_init())
+        return -1;
 
-    signal_pipe_add_signal (SIGINT);
-    signal_pipe_add_signal (SIGTERM);
-    signal_pipe_add_signal (SIGKILL);
-    return signal_pipe_attach_glib (spgqok_handler, _mainloop);
+    signal_pipe_add_signal(SIGINT);
+    signal_pipe_add_signal(SIGTERM);
+    signal_pipe_add_signal(SIGKILL);
+    return signal_pipe_attach_glib(spgqok_handler, _mainloop);
 }
 
-static int
-lcm_message_ready (GIOChannel *source, GIOCondition cond, lcm_t *lcm)
+static int lcm_message_ready(GIOChannel *source, GIOCondition cond, lcm_t *lcm)
 {
-    if (0 != lcm_handle (lcm)) {
+    if (0 != lcm_handle(lcm)) {
         g_main_loop_quit(_mainloop);
         return FALSE;
     }
@@ -175,83 +167,77 @@ typedef struct {
 static GHashTable *lcm_glib_sources = NULL;
 static GStaticMutex lcm_glib_sources_mutex = G_STATIC_MUTEX_INIT;
 
-int
-glib_mainloop_attach_lcm (lcm_t *lcm)
+int glib_mainloop_attach_lcm(lcm_t *lcm)
 {
-    g_static_mutex_lock (&lcm_glib_sources_mutex);
+    g_static_mutex_lock(&lcm_glib_sources_mutex);
 
     if (!lcm_glib_sources) {
-        lcm_glib_sources = g_hash_table_new (g_direct_hash, g_direct_equal);
+        lcm_glib_sources = g_hash_table_new(g_direct_hash, g_direct_equal);
     }
 
-    if (g_hash_table_lookup (lcm_glib_sources, lcm)) {
-        g_static_mutex_unlock (&lcm_glib_sources_mutex);
+    if (g_hash_table_lookup(lcm_glib_sources, lcm)) {
+        g_static_mutex_unlock(&lcm_glib_sources_mutex);
         return -1;
     }
 
-    glib_attached_lcm_t *galcm = 
-        (glib_attached_lcm_t*) calloc (1, sizeof (glib_attached_lcm_t));
+    glib_attached_lcm_t *galcm = (glib_attached_lcm_t *) calloc(1, sizeof(glib_attached_lcm_t));
 
-    galcm->ioc = g_io_channel_unix_new (lcm_get_fileno (lcm));
-    galcm->sid = g_io_add_watch (galcm->ioc, G_IO_IN, (GIOFunc) lcm_message_ready, 
-            lcm);
+    galcm->ioc = g_io_channel_unix_new(lcm_get_fileno(lcm));
+    galcm->sid = g_io_add_watch(galcm->ioc, G_IO_IN, (GIOFunc) lcm_message_ready, lcm);
     galcm->lcm = lcm;
 
-    g_hash_table_insert (lcm_glib_sources, lcm, galcm);
+    g_hash_table_insert(lcm_glib_sources, lcm, galcm);
 
-    g_static_mutex_unlock (&lcm_glib_sources_mutex);
+    g_static_mutex_unlock(&lcm_glib_sources_mutex);
     return 0;
 }
 
-int
-glib_mainloop_detach_lcm (lcm_t *lcm)
+int glib_mainloop_detach_lcm(lcm_t *lcm)
 {
-    g_static_mutex_lock (&lcm_glib_sources_mutex);
+    g_static_mutex_lock(&lcm_glib_sources_mutex);
     if (!lcm_glib_sources) {
-        g_static_mutex_unlock (&lcm_glib_sources_mutex);
+        g_static_mutex_unlock(&lcm_glib_sources_mutex);
         return -1;
     }
 
-    glib_attached_lcm_t *galcm = 
-        (glib_attached_lcm_t*) g_hash_table_lookup (lcm_glib_sources, lcm);
+    glib_attached_lcm_t *galcm = (glib_attached_lcm_t *) g_hash_table_lookup(lcm_glib_sources, lcm);
 
     if (!galcm) {
-        g_static_mutex_unlock (&lcm_glib_sources_mutex);
+        g_static_mutex_unlock(&lcm_glib_sources_mutex);
         return -1;
     }
 
-    g_io_channel_unref (galcm->ioc);
-    g_source_remove (galcm->sid);
+    g_io_channel_unref(galcm->ioc);
+    g_source_remove(galcm->sid);
 
-    g_hash_table_remove (lcm_glib_sources, lcm);
-    free (galcm);
+    g_hash_table_remove(lcm_glib_sources, lcm);
+    free(galcm);
 
-    if (g_hash_table_size (lcm_glib_sources) == 0) {
-        g_hash_table_destroy (lcm_glib_sources);
+    if (g_hash_table_size(lcm_glib_sources) == 0) {
+        g_hash_table_destroy(lcm_glib_sources);
         lcm_glib_sources = NULL;
     }
 
-    g_static_mutex_unlock (&lcm_glib_sources_mutex);
+    g_static_mutex_unlock(&lcm_glib_sources_mutex);
     return 0;
 }
 
-void 
-mkdir_with_parents (const char *path, int mode)
+void mkdir_with_parents(const char *path, int mode)
 {
 #ifdef WIN32
-	g_mkdir_with_parents(path, mode);
+    g_mkdir_with_parents(path, mode);
 #else
     int len = strlen(path);
     for (int i = 0; i < len; i++) {
-        if (path[i]=='/') {
-            char *dirpath = (char *) malloc(i+1);
+        if (path[i] == '/') {
+            char *dirpath = (char *) malloc(i + 1);
             strncpy(dirpath, path, i);
-            dirpath[i]=0;
+            dirpath[i] = 0;
 
             mkdir(dirpath, mode);
             free(dirpath);
 
-            i++; // skip the '/'
+            i++;  // skip the '/'
         }
     }
     mkdir(path, mode);
--- lcm-logger/lcm_logplayer.c
+++ lcm-logger/lcm_logplayer.c
@@ -1,36 +1,33 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
 
 #include <string.h>
 
 #include <lcm/lcm.h>
 
-
 typedef struct logplayer logplayer_t;
-struct logplayer
-{
-    lcm_t * lcm_in;
-    lcm_t * lcm_out;
+struct logplayer {
+    lcm_t *lcm_in;
+    lcm_t *lcm_out;
     int verbose;
 };
 
-void
-handler (const lcm_recv_buf_t *rbuf, const char *channel, void *u)
+void handler(const lcm_recv_buf_t *rbuf, const char *channel, void *u)
 {
-    logplayer_t * l = (logplayer_t *) u;
+    logplayer_t *l = (logplayer_t *) u;
 
     if (l->verbose)
-        printf ("%.3f Channel %-20s size %d\n", rbuf->recv_utime / 1000000.0,
-                channel, rbuf->data_size);
+        printf("%.3f Channel %-20s size %d\n", rbuf->recv_utime / 1000000.0, channel,
+               rbuf->data_size);
 
-    lcm_publish (l->lcm_out, channel, rbuf->data, rbuf->data_size);
+    lcm_publish(l->lcm_out, channel, rbuf->data, rbuf->data_size);
 }
 
-static void
-usage (char * cmd)
+static void usage(char *cmd)
 {
-    fprintf (stderr, "\
+    fprintf(stderr,
+            "\
 Usage: %s [OPTION...] FILE\n\
   Reads packets from an LCM log file and publishes them to LCM.\n\
 \n\
@@ -40,16 +37,16 @@ Options:\n\
   -e, --regexp=EXPR   GLib regular expression of channels to play.\n\
   -l, --lcm-url=URL   Play logged messages on the specified LCM URL.\n\
   -h, --help          Shows some help text and exits.\n\
-  \n", cmd);
+  \n",
+            cmd);
 }
 
-int
-main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
     logplayer_t l;
     double speed = 1.0;
     int c;
-    char * expression = NULL;
+    char *expression = NULL;
     struct option long_opts[] = {
         {"help", no_argument, 0, 'h'},          {"speed", required_argument, 0, 's'},
         {"lcm-url", required_argument, 0, 'l'}, {"verbose", no_argument, 0, 'v'},
@@ -57,67 +54,66 @@ main(int argc, char ** argv)
     };
 
     char *lcmurl = NULL;
-    memset (&l, 0, sizeof (logplayer_t));
-    while ((c = getopt_long (argc, argv, "hp:s:ve:l:", long_opts, 0)) >= 0)
-    {
+    memset(&l, 0, sizeof(logplayer_t));
+    while ((c = getopt_long(argc, argv, "hp:s:ve:l:", long_opts, 0)) >= 0) {
         switch (c) {
-            case 's':
-                speed = strtod (optarg, NULL);
-                break;
-            case 'l':
-                free(lcmurl);
-                lcmurl = strdup(optarg);
-                break;
-            case 'v':
-                l.verbose = 1;
-                break;
-            case 'e':
-                expression = strdup (optarg);
-                break;
-            case 'h':
-            default:
-                usage (argv[0]);
-                return 1;
+        case 's':
+            speed = strtod(optarg, NULL);
+            break;
+        case 'l':
+            free(lcmurl);
+            lcmurl = strdup(optarg);
+            break;
+        case 'v':
+            l.verbose = 1;
+            break;
+        case 'e':
+            expression = strdup(optarg);
+            break;
+        case 'h':
+        default:
+            usage(argv[0]);
+            return 1;
         };
     }
 
     if (optind != argc - 1) {
-        usage (argv[0]);
+        usage(argv[0]);
         return 1;
     }
 
-    char * file = argv[optind];
-    printf ("Using playback speed %f\n", speed);
+    char *file = argv[optind];
+    printf("Using playback speed %f\n", speed);
     if (!expression)
-        expression = strdup (".*");
+        expression = strdup(".*");
 #ifndef WIN32
     char url_in[strlen(file) + 64];
 #else
     char url_in[2048];
 #endif
-    sprintf (url_in, "file://%s?speed=%f", argv[optind], speed);
-    l.lcm_in = lcm_create (url_in);
+    sprintf(url_in, "file://%s?speed=%f", argv[optind], speed);
+    l.lcm_in = lcm_create(url_in);
     if (!l.lcm_in) {
-        fprintf (stderr, "Error: Failed to open %s\n", file);
+        fprintf(stderr, "Error: Failed to open %s\n", file);
         free(expression);
         return 1;
     }
 
-    l.lcm_out = lcm_create (lcmurl);
+    l.lcm_out = lcm_create(lcmurl);
     free(lcmurl);
     if (!l.lcm_out) {
-        fprintf (stderr, "Error: Failed to create LCM\n");
+        fprintf(stderr, "Error: Failed to create LCM\n");
         free(expression);
         return 1;
     }
 
-    lcm_subscribe (l.lcm_in, expression, handler, &l);
+    lcm_subscribe(l.lcm_in, expression, handler, &l);
 
     while (!lcm_handle(l.lcm_in)) {
         // Do nothing
     }
 
-    lcm_destroy (l.lcm_in);
-    lcm_destroy (l.lcm_out);
-    free (expression);
+    lcm_destroy(l.lcm_in);
+    lcm_destroy(l.lcm_out);
+    free(expression);
 }
