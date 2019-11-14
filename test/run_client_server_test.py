8bc90b8eaf00777393eed0db46e4e3625d1ec1da
Albert Huang <ashuang@gmail.com>
2019-11-14 11:18:10 -0800

    test wait for server to start

--- test/run_client_server_test.py
+++ test/run_client_server_test.py
@@ -2,13 +2,18 @@
 import os
 import sys
 import subprocess
+import time
 
 def main(server, *client):
     # Start the test server
     print("Starting test server")
     server_proc = subprocess.Popen(server)
 
+    # Kludge. Wait for server to start.
+    time.sleep(1)
+
     # Run the client tests while the test server is running
+    print("Starting test client")
     test_result = subprocess.call(client)
 
     # Stop the test server
